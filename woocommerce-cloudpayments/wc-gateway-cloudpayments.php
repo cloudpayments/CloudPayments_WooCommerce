6acd8b07ac07ba744d0a0b62c55870f561ef657f
Victor Cha <orky@bk.ru>
2016-05-26 14:01:53 +0300

    Fix for no getallheaders error, when using nginx

--- woocommerce-cloudpayments/wc-gateway-cloudpayments.php
+++ woocommerce-cloudpayments/wc-gateway-cloudpayments.php
@@ -9,6 +9,25 @@ Author URI: https://vk.com/kosteg_benko
 */
 if ( ! defined( 'ABSPATH' ) ) exit;
 
+// if ngnix, not Apache 
+
+if (!function_exists('getallheaders'))  {
+    function getallheaders()
+    {
+        if (!is_array($_SERVER)) {
+            return array();
+        }
+
+        $headers = array();
+        foreach ($_SERVER as $name => $value) {
+            if (substr($name, 0, 5) == 'HTTP_') {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
+    }
+}
+
 add_action('plugins_loaded', 'CloudPayments', 0);
 function CloudPayments() {	
 	if ( !class_exists( 'WC_Payment_Gateway' ) ) return;
