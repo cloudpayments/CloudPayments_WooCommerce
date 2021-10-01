a00972a36edb89bd0f4fc3dc4a03437a4f4cd52f
Yunus Gaziev <yunusga@yandex.ru>
2021-10-01 13:09:02 +0500

    add(filter): cloudpayments_send_receipt_item

--- cloudpayments-gateway-for-woocommerce/inc/wc-cloud-payments-gateway.php
+++ cloudpayments-gateway-for-woocommerce/inc/wc-cloud-payments-gateway.php
@@ -533,21 +533,31 @@ class WC_CloudPayments_Gateway extends WC_Payment_Gateway {
 		$cart         = $order->get_items();
 		$total_amount = 0;
 
-		foreach ( $cart as $item_data ) {
+		foreach ( $cart as $item_id => $item_data ) {
 			$product = $item_data->get_product();
+
 			if ( 'wc-' . $new_status == $this->status_delivered ) {
 				$method = 4;
 			} else {
 				$method = (int) $this->kassa_method;
 			}
-			$items[] = array(
-				'label'    => $product->get_name(),
-				'price'    => number_format( $product->get_price(), 2, '.', '' ),
-				'quantity' => $item_data->get_quantity(),
-				'amount'   => number_format( floatval( $item_data->get_total() ), 2, '.', '' ),
-				'vat'      => $this->kassa_taxtype,
-				'method'   => $method,
-				'object'   => (int) $this->kassa_object,
+
+			$items[] = apply_filters(
+				'cloudpayments_send_receipt_item',
+				array(
+					'label'    => $product->get_name(),
+					'price'    => number_format( $product->get_price(), 2, '.', '' ),
+					'quantity' => $item_data->get_quantity(),
+					'amount'   => number_format( floatval( $item_data->get_total() ), 2, '.', '' ),
+					'vat'      => $this->kassa_taxtype,
+					'method'   => $method,
+					'object'   => (int) $this->kassa_object,
+				),
+				$product,
+				$item_id,
+				$item_data,
+				$method,
+				$this
 			);
 
 			$total_amount = $total_amount + number_format( floatval( $item_data->get_total() ), 2, '.', '' );
