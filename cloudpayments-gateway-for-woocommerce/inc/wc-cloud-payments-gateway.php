1e30cf678955ff4a00c94785bab7f06683fdfa13
Yunus Gaziev <yunusga@yandex.ru>
2021-10-21 16:40:44 +0500

    add(filter): cloudpayments_process_payment_shipping_data Фильтр данных о доставке и налогах перед отправкой в платёжный сервис

--- cloudpayments-gateway-for-woocommerce/inc/wc-cloud-payments-gateway.php
+++ cloudpayments-gateway-for-woocommerce/inc/wc-cloud-payments-gateway.php
@@ -94,16 +94,21 @@ class WC_CloudPayments_Gateway extends WC_Payment_Gateway
         $title         = array();
         $items_array   = array();
         $items         = $order->get_items();
-        $shipping_data = array(
-            "label"    => "Доставка",
-            "price"    => number_format((float)$order->get_total_shipping() + abs((float)$order->get_shipping_tax()), 2, '.', ''),
-            "quantity" => "1.00",
-            "amount"   => number_format((float)$order->get_total_shipping() + abs((float)$order->get_shipping_tax()), 2, '.', ''),
-            "vat"      => ($this->delivery_taxtype == "null") ? null : $this->delivery_taxtype,
-            'method'   => (int)$this->kassa_method,
-            'object'   => 4,
-            "ean"      => null
-        );
+		$shipping_data = apply_filters(
+			'cloudpayments_process_payment_shipping_data',
+			array(
+				'label'    => 'Доставка',
+				'price'    => number_format( (float) $order->get_total_shipping() + abs( (float) $order->get_shipping_tax() ), 2, '.', '' ),
+				'quantity' => '1.00',
+				'amount'   => number_format( (float) $order->get_total_shipping() + abs( (float) $order->get_shipping_tax() ), 2, '.', '' ),
+				'vat'      => ( 'null' == $this->delivery_taxtype ) ? null : $this->delivery_taxtype,
+				'method'   => (int) $this->kassa_method,
+				'object'   => 4,
+				'ean'      => null,
+			),
+			$order,
+			$this
+		);
         
         foreach ($items as $item) {
             if ($this->kassa_enabled == 'yes') {
